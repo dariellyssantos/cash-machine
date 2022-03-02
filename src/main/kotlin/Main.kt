fee7448af90ba069e0aaba45223cde8f40c36acc
dariellysssantos <darielly.santos@creditas.com>
2022-03-02 13:58:45 -0300

    fix: changed input logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,21 @@ fun main() {
     val amount = readLine()
     val amountNumber= amount?.toInt()
     if (amountNumber != null) {
-        if (amountNumber  > 600) {
+        if (amountNumber  > 600 || amountNumber<10) {
             println (" valor não permito")
         }else{
-            val arrayCurrencyValue: IntArray= intArrayOf(100,50,10,5,1)
-            arrayCurrencyValue.forEach { index -> println(index )}
-
             println ("O valor do saque será $amount reais")
+            val arrayCurrencyValue: IntArray= intArrayOf(100,50,10,5,1)
+            arrayCurrencyValue.forEach { index ->
+                //verifica se tem nota de 100
+                if (amountNumber%index == 0){
+                    val numberOfNotes = amountNumber/index
+                    return  println("voce receberá $numberOfNotes de notas de $index reais" )
+                }
+            }
+
         }
 
-    }
-}
+    }}
 
 
