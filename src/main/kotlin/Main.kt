4df564841637ed47f7e06ff51fa3126acf3c2576
dariellysssantos <darielly.santos@creditas.com>
2022-03-03 11:31:35 -0300

    feat: implemented part that calculates all grades, with a bug of not calculating the grade of 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,25 @@
 fun main() {
     println("Qual valor deseja sacar?")
-    val amount = readLine()
-    val amountNumber= amount?.toInt()
-    if (amountNumber != null) {
-        if (amountNumber  > 600 || amountNumber<10) {
-            println (" valor não permito")
-        }else{
-            println ("O valor do saque será $amount reais")
-            val arrayCurrencyValue: IntArray= intArrayOf(100,50,10,5,1)
-            arrayCurrencyValue.forEach { index ->
-                //verifica se tem nota de 100
-                if (amountNumber%index == 0){
-                    val numberOfNotes = amountNumber/index
-                    return  println("voce receberá $numberOfNotes de notas de $index reais" )
+    var amount = readLine()
+    var amountNumber= amount?.toInt()
+        if (amountNumber != null) {
+            if (amountNumber  > 600 || amountNumber<10) {
+                println (" valor não permito")
+            }else{
+                println ("O valor do saque será $amount reais")
+                val arrayCurrencyValue: IntArray= intArrayOf(100,50,10,5,1)
+                arrayCurrencyValue.forEach { index ->
+                    val numberOfNotes = amountNumber / index
+                    if (amountNumber % index != 0) {
+                        println("voce receberá $numberOfNotes de notas de $index reais")
+                    }
+
+                    amountNumber -= (index * numberOfNotes)
+
                 }
+
             }
 
         }
-
-    }}
-
+    }
 
