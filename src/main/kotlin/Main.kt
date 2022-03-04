49eed91fa5037b42d2fab10c792ae3cb430776e0
dariellysssantos <darielly.santos@creditas.com>
2022-03-04 15:04:54 -0300

    feat: refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,47 @@
 fun main() {
-    println("Qual valor deseja sacar?")
-    var amount = readLine()
-    var amountNumber = amount?.toInt()
 
-    if (amountNumber != null) {
-        if (amountNumber > 600 || amountNumber < 10 ) {
-            println(" valor não permitido")
-        } else {
-            println("O valor do saque será $amount reais")
+    println("-----Caixa eletronico -----")
+    println("---------------------------")
+    println(" Qual valor deseja sacar?  ")
 
-            val arrayCurrencyValue: IntArray = intArrayOf(100, 50, 10, 5, 1)
+    var amount = readLine()?.toInt()
+    if (amount != null) {
+        validWithdrawalAmount(amount)
+        noteQuantity(amount)
+    } else {
+        println("Informe um valor")
+    }
 
-            arrayCurrencyValue.forEach { index ->
-                val numberOfNotes = amountNumber / index
+}
 
-                if (amountNumber % index == 0) {
-                    return println("voce receberá $numberOfNotes de notas de $index reais")
-                }
-
-                if (amountNumber % index != 0 && amountNumber/index>0) {
-                    println("voce receberá $numberOfNotes de notas de $index reais")
-                }
-
-                amountNumber -= (index * numberOfNotes)
-            }
-
-        }
-
+fun validWithdrawalAmount(amount: Int) {
+    return if (amount > 600 || amount < 10) {
+        println(" valor não permitido")
+    } else {
+        println("O valor do saque será $amount reais")
     }
 }
 
+fun checkDifferentCurrencies(numberOfNotes: Int, amount: Int, index: Int) {
+    if (amount % index == 0) {
+        return println("voce receberá $numberOfNotes de notas de $index reais")
+    }
+    if (amount % index != 0 && amount / index > 0) {
+        println("voce receberá $numberOfNotes de notas de $index reais")
+    }
+}
+
+fun returnRemainingValue(amount: Int, index: Int, numberOfNotes: Int): Int {
+    return amount - (index * numberOfNotes)
+}
+
+fun noteQuantity(amount:Int) {
+    val arrayCurrencyValue: IntArray = intArrayOf(100, 50, 10, 5, 1)
+
+       arrayCurrencyValue.forEach { index ->
+        val numberOfNotes = amount / index
+        checkDifferentCurrencies(numberOfNotes, amount, index)
+        amount = returnRemainingValue(amount, index, numberOfNotes)
+    }
+    main()
+}
